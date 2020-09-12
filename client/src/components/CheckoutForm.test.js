d462751681848eac9d07e2bf430520cc76e006bd
ZackNemec <zacknemec@live.com>
2020-09-11 20:47:55 -0700

    working on last test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, screen, getByText, render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  const { getByText } = render(<CheckoutForm />);
+  const header = getByText(/Checkout Form/);
+  expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+  const button = screen.getByRole("button");
+  fireEvent.click(button);
+  const success = screen.getByText(/You have ordered some plants! Woo-hoo!/i);
+  expext(success).toBeInTheDocument();
+});
